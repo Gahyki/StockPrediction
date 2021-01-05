04b4edc69fc96a4842ca810b5b8c395003457921
Jason <jason.k99@hotmail.com>
2021-01-04 21:26:38 -0500

    WIP calculation of outputs

--- StockPrediction.py
+++ StockPrediction.py
@@ -206,3 +206,27 @@ multi_cell = tf.keras.layers.StackedRNNCells(lstm_cells)
 
 w = tf.compat.v1.get_variable('w',shape=[num_nodes[-1], 1], initializer=tf.keras.initializers.GlorotUniform())
 b = tf.compat.v1.get_variable('b',initializer=tf.random.uniform([1],-0.1,0.1))
+
+# Create cell state and hidden state variables to maintain the state of the LSTM
+c, h = [],[]
+initial_state = []
+for li in range(n_layers):
+    c.append(tf.Variable(tf.zeros([batch_size, num_nodes[li]]), trainable=False))
+    h.append(tf.Variable(tf.zeros([batch_size, num_nodes[li]]), trainable=False))
+    initial_state.append(tf.compat.v1.nn.rnn_cell.LSTMStateTuple(c[li], h[li]))
+
+
+# Do several tensor transofmations, because the function dynamic_rnn requires the output to be of
+# a specific format. Read more at: https://www.tensorflow.org/api_docs/python/tf/nn/dynamic_rnn
+all_inputs = tf.concat([tf.expand_dims(t, 0) for t in train_inputs], axis=0)
+
+# # all_outputs is [seq_length, batch_size, num_nodes]
+# # all_lstm_outputs, state = tf.keras.layers.RNN(
+# #     drop_multi_cell, all_inputs, initial_state=tuple(initial_state),
+# #     time_major=True, dtype=tf.float32)
+# #
+# # all_lstm_outputs = tf.reshape(all_lstm_outputs, [batch_size*num_unrollings, num_nodes[-1]])
+# #
+# # all_outputs = tf.compat.v1.nn.xw_plus_b(all_lstm_outputs, w, b)
+# #
+# # split_outputs = tf.split(all_outputs, num_unrollings, axis=0)
